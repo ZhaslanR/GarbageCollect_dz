646ba503745a2ca6160f1b1bfa09f2f3813116b5
ZhaslanR <zhaslan_12@bk.ru>
2017-12-29 15:05:14 +0600

    Добавьте файлы проекта.

---
+++ GarbageColector/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageColector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test person = new Test() { Name = "Zhaslan", Age = 17 };
+           
+            Console.WriteLine("Поколения класса Test: " + GC.GetGeneration(person));
+            GC.Collect();
+            Console.WriteLine("Поколения класса Test: " + GC.GetGeneration(person));
+
+            Console.ReadLine();
+        }
+    }
+}
